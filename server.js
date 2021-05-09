6909d0ac59a4fc123dbcdf582b73a720b6c5fb77
Awais Ul Hassan <awaisul519@gmail.com>
2021-05-10 00:28:07 +0500

    "INITIAL COMMIT"

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.get('/', (req,res)=>res.send('API RUNNING'));
+app.get('/', (req,res)=>res.send('API RUNNING')); 
 
 const port = process.env.PORT || 5000;
 
