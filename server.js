7dca791d5ef95b1c2b8455c6329c2790695f9a9d
Awais Ul Hassan <awaisul519@gmail.com>
2021-05-11 17:30:01 +0500

    "USER REGISTERED FLOW"

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const connectDb = require('./config/db');
+const express = require("express");
+const connectDb = require("./config/db");
 
 const app = express();
 //CONENCTING THE DATABASE
 connectDb();
 
+app.get("/", (req, res) => res.send("API RUNNING NOW..."));
 
-app.get('/', (req,res)=>res.send('API RUNNING NOW...')); 
+//INIT MIDDLEWARE. To get the body
+app.use(express.json({ extended: false }));
 
-app.use('/api/users' , require('./routes/api/users'));
-app.use('/api/profile' , require('./routes/api/profile'));
-app.use('/api/posts' , require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 //app.use('/api/auth' , require('./routes/api/auth'));
-
-
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log (`Server Started on Port ${port}`));
+app.listen(port, () => console.log(`Server Started on Port ${port}`));
