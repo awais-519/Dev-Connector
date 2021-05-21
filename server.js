e97a9edd44a09b65f2a690671c66d19f2e29adeb
Awais Ul Hassan <awaisul519@gmail.com>
2021-05-21 08:56:39 +0500

    AUTHENTICATE THE USER WITH THE TOKEN.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDb = require("./config/db");
 
 const app = express();
+
 //CONENCTING THE DATABASE
 connectDb();
 
@@ -13,7 +14,7 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-//app.use('/api/auth' , require('./routes/api/auth'));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const port = process.env.PORT || 5000;
 
