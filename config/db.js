dc8b5b891be656979cbe19a5c44c569979c4ac35
Awais Ul Hassan <awaisul519@gmail.com>
2021-05-22 16:36:26 +0500

    Create/Update Profile

--- config/db.js
+++ config/db.js
@@ -1,22 +1,22 @@
-const mangoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mangoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
-
-const connectDb = async () =>{
-    try
-    {
-        await mangoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-        console.log('Mongo db connected');
- 
-    }
-    catch(err)
-    {
-        console.error(err);
-        //EXIT PROCESS WITH FAILURE
-        process.exit(1);
-    }
-} 
+const connectDb = async () => {
+	try {
+		await mangoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+		console.log("Mongo db connected");
+	} catch (err) {
+		console.error(err);
+		//EXIT PROCESS WITH FAILURE
+		process.exit(1);
+	}
+};
 
 //EXPORTING THE FUNCTION SO THAT TO IMPORT LATER ON
-module.exports  = connectDb;      
+module.exports = connectDb;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Profile = require("../../models/Profile");
 const User = require("../../models/Users");
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 //@ROUTE GET API/PROFILE/ME
 //DESC GET CURRENT USER PROFILE
@@ -25,5 +26,92 @@ router.get("/me", auth, async (req, res) => {
 		res.status(500).send({ msg: "SERVER ERROR" });
 	}
 });
+
+//@ROUTE: POST API/PROFILE/ME
+//@DESC:  CREATE/update PROFILE
+//@ACCESS PRIVATE
+router.post(
+	"/me",
+	[
+		auth,
+		[
+			check("skills", "Skills field cannot be empty.").not().isEmpty(),
+			check("status", "Status field cannot be empty.").not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			githubUserName,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
+
+		//BUILD PROFILE OBJECT
+		const profileFields = {};
+		profileFields.user = req.user.id;
+
+		if (company) profileFields.company = company;
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubUserName) profileFields.githubUserName = githubUserName;
+
+		if (skills) {
+			profileFields.skills = skills.split(",").map((skill) => skill.trim());
+		}
+
+		//BUILD SOCIALS OBJECT
+		profileFields.social = {};
+		if (youtube) profileFields.social.youtube = youtube;
+		if (twitter) profileFields.social.twitter = twitter;
+		if (facebook) profileFields.social.facebook = facebook;
+		if (linkedin) profileFields.social.linkedin = linkedin;
+		if (instagram) profileFields.social.instagram = instagram;
+
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+
+			if (profile) {
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				return res.json({ profile });
+			}
+			//CREATE A NEW PROFILE
+			profile = new Profile(profileFields);
+			await profile.save();
+
+			return res.json(profile);
+		} catch (e) {
+			console.log(e);
+			res.status(500).send("Server Error");
+		}
+
+		return res.json(profile);
+	}
+);
+
+//@ROUTE: GET API/PROFILE
+//@DESC:  GET ALL PROFILES
+//@ACCESS PUBLIC
+
+router.get("/", async (req, res) => {});
 
 module.exports = router;
